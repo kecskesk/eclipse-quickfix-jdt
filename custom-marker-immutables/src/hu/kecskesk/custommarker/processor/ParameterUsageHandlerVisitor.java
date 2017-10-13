56e80ef28a00d255862bd615eadc323034bccd31
Krisztián Kecskés <kecskesk@sonrisa.hu>
2017-10-13 22:48:12 +0200

    created immutables project

---
+++ custom-marker-immutables/src/hu/kecskesk/custommarker/processor/ParameterUsageHandlerVisitor.java
@@ -0,0 +1,111 @@
+package hu.kecskesk.custommarker.processor;
+
+import java.util.Optional;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.PrefixExpression;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+import hu.kecskesk.utils.Utils;
+
+public class ParameterUsageHandlerVisitor extends ASTVisitor {
+	private final SingleVariableDeclaration oldParameter;
+	private final ASTRewrite rewrite;
+
+	public ParameterUsageHandlerVisitor(SingleVariableDeclaration oldParameter, ASTRewrite rewrite) {
+		this.oldParameter = oldParameter;
+		this.rewrite = rewrite;
+	}
+
+	@Override
+	public boolean visit(SimpleName node) {
+		IBinding bindingVisited = node.resolveBinding();
+		IBinding bindingParameter = oldParameter.getName().resolveBinding();
+
+		if (bindingVisited.equals(bindingParameter)) {
+			handleUsageOfParameter(node, rewrite);
+		}
+		return false;
+	}
+
+	private void handleUsageOfParameter(SimpleName node, ASTRewrite rewrite) {
+		ASTNode parentNode = node.getParent();
+		if (parentNode instanceof InfixExpression) {
+			handleInfixExpression(node, rewrite, (InfixExpression) parentNode);
+		} else if (parentNode instanceof VariableDeclarationFragment || parentNode instanceof MethodInvocation
+				|| parentNode instanceof SwitchStatement || parentNode instanceof ReturnStatement) {
+			handleByReplaceWithGet(node, rewrite);
+		} else if (parentNode instanceof Assignment) {
+			handleAssignment(node, rewrite, (Assignment) parentNode);
+		} else if (parentNode instanceof SingleVariableDeclaration) {
+			// this is the parameter itself, we don't need to care about it
+		} else {
+			unknownCodePart(parentNode);
+		}
+	}
+
+	private void unknownCodePart(ASTNode parentNode) {
+		System.out.println("!!!!!!!! WARNING NEW USECASE: ");
+		System.out.println(parentNode);
+		System.out.println(parentNode.getClass().getName());
+	}
+
+	private void handleInfixExpression(SimpleName node, ASTRewrite rewrite, InfixExpression expression) {
+		Optional<SimpleName> var = Utils.getVariableIfNullCheck(expression);
+		if (var.isPresent() && var.get() == node) {
+
+			AST ast = rewrite.getAST();
+			MethodInvocation isPresentCall = ast.newMethodInvocation();
+			isPresentCall.setName(ast.newSimpleName("isPresent"));
+			isPresentCall.setExpression(ast.newSimpleName(node.getIdentifier()));
+
+			if (expression.getOperator().equals(InfixExpression.Operator.EQUALS)) {
+				rewrite.replace(expression, isPresentCall, null);
+			} else if (expression.getOperator().equals(InfixExpression.Operator.NOT_EQUALS)) {
+				PrefixExpression negationExpression = ast.newPrefixExpression();
+				negationExpression.setOperator(PrefixExpression.Operator.NOT);
+				negationExpression.setOperand(isPresentCall);
+				rewrite.replace(expression, negationExpression, null);
+			}
+		} else {
+			unknownCodePart(expression);
+		}
+	}
+
+	private void handleByReplaceWithGet(SimpleName node, ASTRewrite rewrite) {
+		AST ast = rewrite.getAST();
+		MethodInvocation getCall = ast.newMethodInvocation();
+		getCall.setName(ast.newSimpleName("get"));
+		getCall.setExpression(ast.newSimpleName(node.getIdentifier()));
+		rewrite.replace(node, getCall, null);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void handleAssignment(SimpleName node, ASTRewrite rewrite, Assignment expression) {
+		if (expression.getLeftHandSide() == node) {
+			AST ast = rewrite.getAST();
+			Expression originalRightHandSide = expression.getRightHandSide();
+
+			MethodInvocation ofWrapper = ast.newMethodInvocation();
+			ofWrapper.setName(ast.newSimpleName("of"));
+			ofWrapper.setExpression(ast.newSimpleName("Optional"));
+			ofWrapper.arguments().add(rewrite.createCopyTarget(originalRightHandSide));
+
+			rewrite.replace(originalRightHandSide, ofWrapper, null);
+		} else {
+			handleByReplaceWithGet(node, rewrite);
+		}
+	}
+}
---
+++ custom-marker-immutables/src/hu/kecskesk/utils/Utils.java
@@ -0,0 +1,45 @@
+package hu.kecskesk.utils;
+
+import java.util.Optional;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.SimpleName;
+
+public class Utils {
+	public static Optional<SimpleName> getVariableIfNullCheck(InfixExpression infixExpression) {
+		if (isNull(infixExpression.getLeftOperand())) {
+			// null == variable
+			if (isSimpleName(infixExpression.getRightOperand())) {
+				return Optional.of((SimpleName) infixExpression.getRightOperand());
+			}
+		} else if (isNull(infixExpression.getRightOperand())) {
+			// variable == null
+			if (isSimpleName(infixExpression.getLeftOperand())) {
+				return Optional.of((SimpleName) infixExpression.getLeftOperand());
+			}
+		}
+		return Optional.empty();
+	}
+
+	public static boolean isNull(Expression exp) {
+		return ASTNode.NULL_LITERAL == exp.getNodeType();
+	}
+
+	public static boolean isSimpleName(Expression exp) {
+		return ASTNode.SIMPLE_NAME == exp.getNodeType();
+	}
+	
+	public static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null);
+	}
+}
